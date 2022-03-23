c1175f4e079085fe66f602369cdbcccd39320609
CPrz21 <faselona@gmail.com>
2022-03-23 08:15:45 -0600

    48-Tarea - Mostrar nombre y número del pokémon

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,23 +3,45 @@ import type { NextPage, GetStaticProps } from "next";
 
 import { Layout } from "../components/layouts";
 import { pokeApi } from "../api";
-import { PokemonListResponse } from "../interfaces";
+import { PokemonListResponse, SmallPokemon } from "../interfaces";
 
-const Home: NextPage = (props) => {
-  console.log("🚀 ~ file: index.tsx ~ line 6 ~ props", props);
+interface Props {
+  pokemons: SmallPokemon[];
+}
+
+const Home: NextPage<Props> = ({ pokemons }) => {
+  console.log("🚀 ~ file: index.tsx ~ line 6 ~ pokemons", pokemons);
   return (
     <Layout title="Pokemon List">
-      <Button color="gradient">Click me</Button>
+      <ul>
+        {pokemons.map(({ id, img, name }) => (
+          <li key={id}>{`#${id} ${name}`}</li>
+        ))}
+      </ul>
     </Layout>
   );
 };
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
-  const { data } = await pokeApi.get<PokemonListResponse>("/pokemon?limit=151");
+  const {
+    data: { results },
+  } = await pokeApi.get<PokemonListResponse>("/pokemon?limit=151");
+
+  const pokemons: SmallPokemon[] = results.map(
+    (pokemon: SmallPokemon, index) => {
+      const id = index + 1;
+      const img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`;
+      return {
+        ...pokemon,
+        id,
+        img,
+      };
+    }
+  );
 
   return {
     props: {
-      pokemons: data.results,
+      pokemons,
     },
   };
 };
