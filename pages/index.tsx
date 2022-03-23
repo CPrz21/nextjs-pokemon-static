5075afa97b17508051b3be321eb51e372ba211d9
CPrz21 <faselona@gmail.com>
2022-03-23 06:25:20 -0600

    45-Next-GetStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,24 @@
 import { Button } from "@nextui-org/react";
-import type { NextPage } from "next";
+import type { NextPage, GetStaticProps } from "next";
 import { Layout } from "../components/layouts";
 
-const Home: NextPage = () => {
+const Home: NextPage = (props) => {
+  console.log("🚀 ~ file: index.tsx ~ line 6 ~ props", props);
   return (
     <Layout title="Pokemon List">
       <Button color="gradient">Click me</Button>
     </Layout>
   );
+};
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  console.log("first");
+
+  return {
+    props: {
+      name: "carlos",
+    },
+  };
 };
 
 export default Home;
