87bbbdf1191273633926d64a14be0ec22ce3c4fa
CPrz21 <faselona@gmail.com>
2022-03-24 19:14:25 -0600

    55-Tarea - Navegación y página de favoritos

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,8 +1,12 @@
-import { Spacer, Text, useTheme } from "@nextui-org/react";
+import NextLink from "next/link";
+import { Link, Spacer, Text, useTheme } from "@nextui-org/react";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 export const Navbar = () => {
+  const { pathname } = useRouter();
   const { theme } = useTheme();
+  const isFavoritePath = pathname === "/favorites";
   return (
     <div
       style={{
@@ -21,14 +25,24 @@ export const Navbar = () => {
         width={70}
         height={70}
       />
-      <Text color="white" h2>
-        P
-      </Text>
-      <Text color="white" h3>
-        ókemon
-      </Text>
+      <NextLink href="/" passHref>
+        <Link>
+          <Text color="white" h2>
+            P
+          </Text>
+          <Text color="white" h3>
+            ókemon
+          </Text>
+        </Link>
+      </NextLink>
       <Spacer css={{ flex: 1 }} />
-      <Text color="white">Favoritos</Text>
+      {!isFavoritePath && (
+        <NextLink href="/favorites" passHref>
+          <Link>
+            <Text color="white">Favoritos</Text>
+          </Link>
+        </NextLink>
+      )}
     </div>
   );
 };
---
+++ pages/favorites/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+import { Layout } from "../../components/layouts";
+
+const FavoritesPage: NextPage = () => {
+  return (
+    <Layout title="Favorite Pokemons">
+      <div>FavoritesPage</div>
+    </Layout>
+  );
+};
+
+export default FavoritesPage;
